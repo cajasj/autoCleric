15f94bea2bb13814bc6351875043cfe9dd50b92b
Your Name <you@example.com>
2018-02-21 00:43:54 -0500

    fixed  multi select created thread for the check table

--- autoResign/autoResign/StudentOverlaps.cs
+++ autoResign/autoResign/StudentOverlaps.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using CefSharp;
 using CefSharp.WinForms;
@@ -12,7 +13,7 @@ namespace autoResign
 {
     class StudentOverlaps:powerSchool   
     {
-
+        int test = 0;
         private string userNameLog = "";
         private string userPass = "";
         private string url = "https://bridgeportedu.powerschool.com/admin/home.html";
@@ -20,7 +21,7 @@ namespace autoResign
         private bool runOnce = false;
         public List<string> studentID = new List<string>();
         public string readTextLine=" ";
-
+        Thread checkPlease;
         private string checkTableLength = @"(function(){
                 if(document.getElementsByTagName('tbody')[1].children.length==0){
                     return false
@@ -28,6 +29,11 @@ namespace autoResign
                     return true
                 }
             })();";
+        private const string multiInputClick = @"(function(){
+            document.getElementsByClassName('dialogDivM')[0].click();
+            document.getElementById('fldName').options[0].value='statenumber';
+
+           })() ";
         public override void loginUser(string name, string logPass)
         {
             int k = 0;
@@ -48,24 +54,15 @@ namespace autoResign
                 studentID.Add(readTextLine);
             }
 
-
-            Console.WriteLine("before multiSelect {0}", studentID);
-            foreach (string textID in studentID)
-            {
-                Console.WriteLine("#{0} index \nid number is: {1} ", k, textID);
-                k++;
-            }
-
+            
             sIDText.Close();
             System.IO.File.WriteAllText(pathState, string.Empty);
 
             base.initChrome();
             base.loadJS(userNameLog, userPass);
             base.loadCheckJS(url);
-            Console.WriteLine("before multiSelect {0}", studentID.Count);
             
             clickMultiSelect();
-            Console.WriteLine("after showdialog");
         }
 
         protected virtual void clickMultiSelect()
@@ -75,15 +72,16 @@ namespace autoResign
             {
                 if (args.Frame.IsMain)
                 {
-                    checkTable();
-
-                    Console.WriteLine("run once in clickMethod is now {0}", runOnce);
-                 
+                    checkPlease=new Thread (() => checkTable());
+                    checkPlease.Start();
+                    while (checkPlease.IsAlive)
+                        Console.WriteLine(checkPlease.IsAlive);
                     if (runOnce == false)
                     {
                         autoMulti(args);
                         inputMulti(args);
                         clickSearch(args);
+                        Console.WriteLine(test++);
                     }
 
                      
@@ -95,18 +93,14 @@ namespace autoResign
         { 
             var multiSelect = string.Format("document.getElementsByClassName('dialogDivM')[0].click();");
             
-                args.Frame.ExecuteJavaScriptAsync(multiSelect);
-            string stateID = "statenumber";
-            var stateIdSelect = string.Format("document.getElementById('fldName').options[0].value={0};", stateID);
-
-            args.Frame.ExecuteJavaScriptAsync(stateIdSelect);
+                args.Frame.ExecuteJavaScriptAsync(multiInputClick);
         }
         private void inputMulti(FrameLoadEndEventArgs args )
         {
-            
+            Console.WriteLine("input multi for each ");
             foreach (var id in studentID)
             {
-                Console.WriteLine("input multi for each ");
+              
                
                
                 var inputID = string.Format("document.getElementById('multiSelVals').value+={0}+'\\n';", id);
@@ -133,7 +127,7 @@ namespace autoResign
                 {
                     runOnce = (bool)response.Result;
                     Console.WriteLine("run once is now {0}", runOnce);
-                    
+                    checkPlease.Abort();
                 }
             });
         }
